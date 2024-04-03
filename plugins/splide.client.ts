833e6434f058c55db371ab5329d190f644d8166d
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2024-04-03 13:17:07 +0400

    оптимизация слайдера

---
+++ plugins/splide.client.ts
@@ -0,0 +1,1 @@
+// @ts-ignoreimport Splide from '@splidejs/vue-splide'import '@splidejs/vue-splide/css/core' // Import Splide CSSexport default defineNuxtPlugin((nuxtApp) => {  nuxtApp.vueApp.use(Splide) // Register Splide as a Vue plugin})
