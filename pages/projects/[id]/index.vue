ac5bbdf85bdaa521bba7585581187b158dd9ac83
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2024-04-09 15:37:11 +0400

    страница проекта слайдер резултьтат

--- pages/projects/[id]/index.vue
+++ pages/projects/[id]/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type Project from '~/types/Projects/Project'
 import projects from '~/data/projects'
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/vue-splide'
 
 definePageMeta({
   title: 'Проект'
@@ -10,8 +11,28 @@ useHead({
 })
 
 const route = useRoute()
-
 const project = ref<Project>()
+const options = {
+  perPage: 4,
+  arrows: true,
+  pagination: false,
+  gap: 20,
+  breakpoints: {
+    1260: {
+      perPage: 4
+    },
+    768: {
+      perPage: 3
+    },
+    540: {
+      perPage: 1,
+      padding: { left: 0, right: 90 },
+      type: 'loop'
+    }
+  }
+}
+
+const isVideoSlide = (slidePath: string): boolean => slidePath.includes('.mp4')
 
 onMounted(() => {
   project.value = projects.find((pr) => pr.id === +route.params.id)
@@ -62,6 +83,49 @@ onMounted(() => {
             </ul>
           </div>
         </div>
+        <ClientOnly>
+          <div class="project-section">
+            <div class="gallery">
+              <h2 class="gallery-title title title-s">Результат</h2>
+              <Splide
+                ref="splide"
+                class="splider"
+                :options="options"
+                :has-track="false"
+              >
+                <SplideTrack>
+                  <SplideSlide
+                    v-for="slide in project.resultGallery"
+                    :key="slide"
+                  >
+                    <div class="slide">
+                      <div v-if="isVideoSlide(slide)" class="slide-video">
+                        <video :src="slide" no-contorls></video>
+                        <div class="slide-icon">
+                          <IconsPlay />
+                        </div>
+                      </div>
+                      <div v-else class="slide-photo">
+                        <NuxtImg :src="slide" sizes="178px" />
+                        <div class="slide-icon">
+                          <IconsEye />
+                        </div>
+                      </div>
+                    </div>
+                  </SplideSlide>
+                </SplideTrack>
+                <div class="splide__arrows">
+                  <button class="splide__arrow splide__arrow--prev">
+                    <IconsArrowLeft />
+                  </button>
+                  <button class="splide__arrow splide__arrow--next">
+                    <IconsArrowRight />
+                  </button>
+                </div>
+              </Splide>
+            </div>
+          </div>
+        </ClientOnly>
       </section>
     </div>
   </div>
@@ -120,6 +184,137 @@ onMounted(() => {
       flex-direction: column;
       gap: 16px;
     }
+  }
+}
+
+.gallery {
+  padding: 20px;
+  border-radius: 20px;
+  background: $text-secondary;
+  color: $text-white;
+
+  &-title {
+    margin: 0 0 20px;
+  }
+}
+
+.splider {
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.slide {
+  @include ibg;
+  width: 178px;
+  aspect-ratio: 1 / 1;
+  height: 100%;
+  border-radius: 20px;
+  overflow: hidden;
+  cursor: pointer;
+
+  @media (max-width: $md1 + px) {
+    width: 199px;
+  }
+  @media (max-width: $md2 + px) {
+    width: 100%;
+  }
+
+  &-video {
+    video {
+      position: absolute;
+      inset: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .slide-icon {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: $bg-white-alpha-20;
+    }
+  }
+
+  &-icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    transition: opacity 0.3s ease;
+
+    svg {
+      width: 32px;
+      height: 32px;
+    }
+  }
+
+  &-photo,
+  &-video {
+    position: absolute;
+    inset: 0;
+  }
+
+  @media (min-width: $md2 + px) {
+    &:hover {
+      .slide-photo:before,
+      .slide-video:before,
+      .slide-icon {
+        opacity: 1;
+      }
+    }
+    .slide-photo:before,
+    .slide-video:before {
+      content: '';
+      position: absolute;
+      inset: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(45, 55, 68, 0.7);
+      display: block;
+      z-index: 1;
+      opacity: 0;
+      transition: opacity 0.3s ease;
+      border-radius: 20px;
+    }
+    .slide-icon {
+      z-index: 2;
+    }
+  }
+}
+
+.splide__arrow {
+  width: 36px;
+  height: 36px;
+  color: $text-white;
+  background: $bg-white-alpha-20;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+
+  &[disabled] {
+    color: $text-white-alpha;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  &--prev {
+    left: 10px;
+  }
+
+  &--next {
+    right: 10px;
   }
 }
 </style>
