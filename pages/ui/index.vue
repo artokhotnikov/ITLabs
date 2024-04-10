528b141e5532ab5d5fd228d4b7ad33df26c939d6
goriori <vladislav.borisov2010@gmail.com>
2024-04-10 10:24:28 +0400

    Clear no use code

--- pages/ui/index.vue
+++ pages/ui/index.vue
@@ -1,31 +1,13 @@
 <script setup lang="ts">
 import { useOpen } from '~/utils/useOpen'
 
-const {
-  isOpen: isOpenCallSpecialist,
-  open: openCallSpecialist,
-  toggle: toggleCallSpecialist
-} = useOpen()
-const {
-  isOpen: isOpenResultSuccessResponse,
-  open: openResultSuccessResponse,
-  toggle: toggleResultSuccessResponse
-} = useOpen()
-const {
-  isOpen: isOpenResultErrorResponse,
-  open: openResultErrorResponse,
-  toggle: toggleResultErrorResponse
-} = useOpen()
-const {
-  isOpen: isOpenQuestion,
-  open: openQuestion,
-  toggle: toggleQuestion
-} = useOpen()
-const {
-  isOpen: isOpenDiscussion,
-  open: openDiscussion,
-  toggle: toggleDiscussion
-} = useOpen()
+const { isOpen: isOpenCallSpecialist, open: openCallSpecialist } = useOpen()
+const { isOpen: isOpenResultSuccessResponse, open: openResultSuccessResponse } =
+  useOpen()
+const { isOpen: isOpenResultErrorResponse, open: openResultErrorResponse } =
+  useOpen()
+const { isOpen: isOpenQuestion, open: openQuestion } = useOpen()
+const { isOpen: isOpenDiscussion, open: openDiscussion } = useOpen()
 const inputModel = ref('')
 </script>
 <template>
