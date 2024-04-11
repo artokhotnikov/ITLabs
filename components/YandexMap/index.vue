57d1a1660a2a629ff5f24a84471601c73d7be0f6
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2024-04-11 12:07:54 +0400

    контакты/яндекс карты

--- components/YandexMap/index.vue
+++ components/YandexMap/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import mapConfig from '~/data/mapConfig.json'
 import { shallowRef } from 'vue'
 import type { YMap } from '@yandex/ymaps3-types'
 import {
@@ -8,76 +9,91 @@ import {
   YandexMapMarker
 } from 'vue-yandex-maps'
 
-const customization = [
-  // Делаем прозрачными все геометрии водных объектов.
-  {
-    tags: {
-      all: ['water']
-    },
-    elements: 'geometry',
-    stylers: [
-      {
-        opacity: 0
-      }
-    ]
-  },
-  // Меняем цвет подписей для всех POI и узлов сети общественного транспорта.
-  {
-    tags: {
-      any: ['poi', 'transit_location']
-    },
-    elements: 'label.text.fill',
-    stylers: [
-      {
-        color: '#0000DD'
-      }
-    ]
-  }
-]
+const { width } = useWindowSize()
+
+const sm = computed(() => width.value <= 767)
+const customization = mapConfig
 //Можно использовать для различных преобразований
 const map = shallowRef<null | YMap>(null)
+const hasAutoRotate = ref(true)
+const mapAzimuth = ref(0)
+const mapTilt = ref((40 * Math.PI) / 180)
+const show = ref(false)
+const frame = ref<null | number>(null)
+const center = ref([53.20704211561477, 56.87940830777453])
+// Automatically rotate the camera
+const startAutoRotationCamera = () => {
+  if (!map.value) return
+
+  if (hasAutoRotate.value) {
+    //  Divide degrees by 100 to slow rotation to ~20 degrees / sec
+    mapAzimuth.value = map.value.azimuth + (10 * Math.PI) / 180 / 100
+    // Request the next frame of the animation
+    frame.value = requestAnimationFrame(startAutoRotationCamera)
+
+    // If the automatic rotation mode is stopped then cancel the request for the next animation frame
+  } else if (frame.value) cancelAnimationFrame(frame.value)
+}
+
+watch(
+  [hasAutoRotate, map],
+  () => {
+    if (typeof requestAnimationFrame === 'undefined') return
+    requestAnimationFrame(startAutoRotationCamera)
+  },
+  {
+    immediate: true
+  }
+)
+
+onBeforeUnmount(() => {
+  if (frame.value) {
+    hasAutoRotate.value = false
+    cancelAnimationFrame(frame.value)
+  }
+})
+onMounted(() => {
+  if (sm.value) {
+    center.value = [53.211524774906344, 56.8799955436624]
+  }
+  setTimeout(() => {
+    show.value = true
+  }, 500)
+})
 </script>
 
 <template>
-  <div class="map">
-    <yandex-map
-      v-model="map"
-      :settings="{
-        location: {
-          center: [53.209376, 56.880786],
-          zoom: 16
-        },
-        theme: 'dark'
-      }"
-      width="100%"
-      height="100%"
+  <yandex-map
+    v-show="show"
+    v-model="map"
+    :settings="{
+      location: {
+        center,
+        zoom: 16.5
+      },
+      camera: {
+        azimuth: mapAzimuth,
+        tilt: mapTilt,
+        duration: hasAutoRotate ? 0 : 250
+      }
+    }"
+    width="100%"
+    height="100%"
+  >
+    <yandex-map-default-scheme-layer :settings="{ customization }" />
+    <yandex-map-default-features-layer />
+    <yandex-map-marker
+      :settings="{ coordinates: [53.21079, 56.880532] }"
+      position="top left-center"
     >
-      <yandex-map-default-scheme-layer :settings="{ customization }" />
-      <yandex-map-default-features-layer />
-      <yandex-map-marker
-        :settings="{ coordinates: [53.21079, 56.880532] }"
-        position="top left-center"
-      >
-        <div class="marker">
-          <img
-            class="pin"
-            src="/assets/img/yandex_map/marker.svg"
-            alt="marker"
-          />
-        </div>
-      </yandex-map-marker>
-    </yandex-map>
-  </div>
+      <div class="marker">
+        <img class="pin" src="/assets/img/yandex_map/marker.svg" alt="marker" />
+      </div>
+    </yandex-map-marker>
+  </yandex-map>
 </template>
 
 <style scoped lang="scss">
-.map {
-  width: 100%;
-  height: 790px;
-  border-radius: 40px;
-  overflow: hidden;
-}
-
 .marker {
   width: 68px;
   height: 74px;
--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -5,15 +5,157 @@ definePageMeta({
 useHead({
   title: 'Контакты'
 })
+
+const show = ref(false)
+onMounted(() => {
+  setTimeout(() => {
+    show.value = true
+  }, 500)
+})
 </script>
 
 <template>
   <div class="page">
     <div class="container">
       <Breadcrumbs />
-      <YandexMap />
+      <div class="page-title title">Контакты</div>
+      <div v-show="show" class="map">
+        <YandexMap />
+        <div class="contact">
+          <h2 class="contact-title title title-xxs">
+            Офис
+            <mark>в Ижевске</mark>
+          </h2>
+          <div class="contact-subtitle text">
+            Ул. Холмогорова, 85, Ижевск, 426011
+          </div>
+          <div class="contact-row">
+            <span> Офис </span>
+            <span>
+              <a href="tel:+7(3412) 000-000" target="_blank">
+                +7(3412) 000-000
+              </a>
+            </span>
+          </div>
+          <div class="contact-row">
+            <span> E-mail </span>
+            <span>
+              <a href="mailto:info@itlabs.top" target="_blank">
+                info@itlabs.top
+              </a>
+            </span>
+          </div>
+          <div class="contact-row">
+            <span> Telegtam </span>
+            <span>
+              <a href="@itlabs" target="_blank">@itlabs</a>
+            </span>
+          </div>
+          <div class="contact-row">
+            <span> VK </span>
+            <span>
+              <a href="your_itlabs" target="_blank">your_itlabs</a>
+            </span>
+          </div>
+          <div class="contact-link medium">
+            <a href="#">Реквизиты компании</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '/assets/scss/variables';
+
+.page {
+  &-title {
+    color: $text-blue;
+    margin: 0 0 40px;
+  }
+}
+
+.map {
+  width: 100%;
+  height: 790px;
+  border-radius: 40px;
+  overflow: hidden;
+  position: relative;
+  @media (max-width: $md3 + px) {
+    height: 790px;
+    border-radius: 0;
+    margin: 0 calc(50% - 50vw);
+    width: 100vw;
+  }
+}
+
+.contact {
+  position: absolute;
+  top: 50%;
+  left: 210px;
+  transform: translateY(-50%);
+  background: $bg-white;
+  padding: 40px;
+  border-radius: 40px;
+  box-shadow: 0 50px 50px 0 rgba(88, 104, 124, 0.1);
+  @media (max-width: $md3 + px) {
+    left: 16px;
+    top: auto;
+    transform: translateY(0);
+    bottom: 16px;
+    border-radius: 20px;
+    width: calc(100% - 32px);
+  }
+
+  &-title {
+    margin: 0 0 40px;
+    @media (max-width: $md3 + px) {
+      margin: 0 0 32px;
+    }
+  }
+
+  &-subtitle {
+    margin: 0 0 40px;
+    color: $text-third;
+    @media (max-width: $md3 + px) {
+      margin: 0 0 32px;
+    }
+  }
+
+  &-row {
+    display: flex;
+    justify-content: space-between;
+    @media (min-width: $md2 + px) {
+      min-width: 314px;
+    }
+
+    &:not(:first-child) {
+      margin: 20px 0 0;
+      @media (max-width: $md3 + px) {
+        margin: 16px 0 0;
+      }
+    }
+
+    span {
+      &:first-child {
+        font-weight: 500;
+        color: $text-secondary;
+      }
+
+      &:last-child {
+        color: $text-third;
+        flex: 0 0 152px;
+        @media (max-width: $md3 + px) {
+          flex: 0 1 180px;
+        }
+      }
+    }
+  }
+
+  &-link {
+    margin: 40px 0 0;
+    color: $text-blue;
+  }
+}
+</style>
