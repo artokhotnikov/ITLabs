2532d57b86fb5c91f12c348690b6339cb636e646
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2024-10-01 17:30:25 +0400

    заглушка на слайдер

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,14 @@ useHead({
 
 <template>
   <div class="page page-main">
-    <ClientOnly>
-      <HomeSlider />
-    </ClientOnly>
+    <div class="page-slider">
+      <ClientOnly>
+        <HomeSlider />
+        <template #fallback>
+          <p>loading..</p>
+        </template>
+      </ClientOnly>
+    </div>
     <HomeCategories />
     <HomeProjects />
     <HomeStages />
@@ -22,4 +27,14 @@ useHead({
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '/assets/scss/variables';
+.page-slider {
+  height: 420px;
+  overflow: hidden;
+
+  @media (max-width: ($md4 + px)) {
+    height: 440px;
+  }
+}
+</style>
